c054e19c01d8cacfd9ef355c3ef5de75bc838c1a
Bret Petersen <bretpetersen82@gmail.com>
2023-10-07 15:46:57 -0500

    Updated image and icon relative URL placements with proper React imports

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,28 @@
+import icon01lg from '../assets/lead-generation.png'
+import icon02ba from '../assets/brand-awareness.png'
+import icon03cm from '../assets/cost-management.png'
+
+
 const Sidebar = () => {
     return (
         <aside>
             <div className="benefit-lead">
                 <h3>Lead Generation</h3>
-                <img src="src/assets/lead-generation.png" />
+                <img src={icon01lg} />
                 <p>
                     Inbound strategies for lead generation require less work for your business, bringing customers directly to your website.
                 </p>
             </div>
             <div className="benefit-brand">
                 <h3>Brand Awareness</h3>
-                <img src="src/assets/brand-awareness.png" />
+                <img src={icon02ba} />
                 <p>
                     Users find your business through paid and organic searches, increasing the search ranking and visibility for your business.
                 </p>
             </div>
             <div className="benefit-cost">
                 <h3>Cost Management</h3>
-                <img src="src/assets/cost-management.png" />
+                <img src={icon03cm} />
                 <p>
                     As the search ranking for your business increases, your advertising costs decrease, and you no longer need to advertise your page.
                 </p>
